473ebe5aff5032f0633241d02a79b8935304fc5d
lucastkd <lucasnatanaeloliveiratkd@gmail.com>
2026-06-01 20:48:23 -0300

    UPDATE - começando

--- pagina.c
+++ pagina.c
@@ -3,31 +3,39 @@
 
 int main (void){
 	
-	int menu;
+	char nome, email;
+	int menu, telefone;
 	
-	printf("escolha a opcao desejada\n 1 - Incluir contato\n 2 - Listar contatos\n 3 - Consultar contato pelo nome\n 4 - Excluir contato\n 5 - Sair\n");
+	printf(" 1 - Incluir contato\n 2 - Listar contatos\n 3 - Consultar contato pelo nome\n 4 - Excluir contato\n 5 - Sair\n");
+	printf("Escolha a opcao desejada: ");
 	scanf("%d", &menu);
 	
 	switch (menu){
 		
-		case 1:
-			printf("voce entrou na opcao 1");
+		case 1: //add contatos
+			printf("Digite seu nome");
+			scanf("%c", &nome);
+			printf("Digite seu email");
+			scanf("%c", &email);
+			printf("Digite seu numero de telefone(sem caracteres especiais)");
+			scanf("%i", &telefone);
 			break;
 		
-		case 2:
+		case 2: //listar contatos
 			printf("voce entrou na opcao 2");
 			break;
 			
-		case 3:
-			printf("voce entrou na opcao 3");
+		case 3: //pesquisar contatos
+			printf("Digite o nome para pesquisa");
+			scanf("%c", &nome);
 			break;
 		
-		case 4:
+		case 4: //excluir contatos
 			printf("voce entrou na opcao 4");
 			break;
 			
-		case 5:
-			printf("voce entrou na opcao 5");
+		case 5: //sair
+			printf("Encerrando.... ");
 			break;
 			
 		default:

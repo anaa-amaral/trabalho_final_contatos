7d5a62ab5aea3dce0a03676cd10cc26ad1a10403
Ana <anaaluizadoamaral@gmail.com>
2026-06-08 19:49:21 -0300

    Add - código completo

--- pagina.c
+++ pagina.c
@@ -1,46 +1,173 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-int main (void){
-	
-	char nome, email;
-	int menu, telefone;
-	
-	printf(" 1 - Incluir contato\n 2 - Listar contatos\n 3 - Consultar contato pelo nome\n 4 - Excluir contato\n 5 - Sair\n");
-	printf("Escolha a opcao desejada: ");
-	scanf("%d", &menu);
-	
-	switch (menu){
-		
-		case 1: //add contatos
-			printf("Digite seu nome");
-			scanf("%c", &nome);
-			printf("Digite seu email");
-			scanf("%c", &email);
-			printf("Digite seu numero de telefone(sem caracteres especiais)");
-			scanf("%i", &telefone);
-			break;
-		
-		case 2: //listar contatos
-			printf("voce entrou na opcao 2");
-			break;
-			
-		case 3: //pesquisar contatos
-			printf("Digite o nome para pesquisa");
-			scanf("%c", &nome);
-			break;
-		
-		case 4: //excluir contatos
-			printf("voce entrou na opcao 4");
-			break;
-			
-		case 5: //sair
-			printf("Encerrando.... ");
-			break;
-			
-		default:
-			printf("opcao invalida");
-	}
-	
-	return 0;
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+typedef struct
+{
+    char nome[50];
+    char telefone[20];
+    char email[50];
+} Contato;
+
+Contato agenda[MAX];
+
+int totalContatos = 0;
+
+void incluirContato()
+{
+    if (totalContatos >= MAX)
+    {
+        printf("\nAgenda cheia!\n");
+        return;
+    }
+
+    printf("\nINCLUIR CONTATO\n");
+
+    printf("Nome: ");
+    scanf(" %[^\n]", agenda[totalContatos].nome);
+
+    printf("Telefone: ");
+    scanf(" %[^\n]", agenda[totalContatos].telefone);
+
+    printf("Email: ");
+    scanf(" %[^\n]", agenda[totalContatos].email);
+
+    totalContatos++;
+
+    printf("\nContato cadastrado com sucesso!\n");
 }
+
+void listarContatos()
+{
+    int i;
+
+    printf("\nLISTA DE CONTATOS\n");
+
+    if (totalContatos == 0)
+    {
+        printf("Nenhum contato cadastrado.\n");
+        return;
+    }
+
+    for (i = 0; i < totalContatos; i++)
+    {
+        printf("\nContato %d\n", i + 1);
+        printf("Nome: %s\n", agenda[i].nome);
+        printf("Telefone: %s\n", agenda[i].telefone);
+        printf("Email: %s\n", agenda[i].email);
+    }
+}
+
+void consultarContato()
+{
+    char nomeBusca[50];
+    int i;
+    int encontrado = 0;
+
+    printf("\nCONSULTAR CONTATO\n");
+
+    printf("Digite o nome: ");
+    scanf(" %[^\n]", nomeBusca);
+
+    for (i = 0; i < totalContatos; i++)
+    {
+        if (strcmp(nomeBusca, agenda[i].nome) == 0)
+        {
+            printf("\nContato encontrado!\n");
+            printf("Nome: %s\n", agenda[i].nome);
+            printf("Telefone: %s\n", agenda[i].telefone);
+            printf("Email: %s\n", agenda[i].email);
+
+            encontrado = 1;
+        }
+    }
+
+    if (encontrado == 0)
+    {
+        printf("\nContato nao encontrado.\n");
+    }
+}
+
+void excluirContato()
+{
+    char nomeExcluir[50];
+    int i, j;
+    int encontrado = 0;
+
+    printf("\nEXCLUIR CONTATO\n");
+
+    printf("Digite o nome do contato: ");
+    scanf(" %[^\n]", nomeExcluir);
+
+    for (i = 0; i < totalContatos; i++)
+    {
+        if (strcmp(nomeExcluir, agenda[i].nome) == 0)
+        {
+            for (j = i; j < totalContatos - 1; j++)
+            {
+                agenda[j] = agenda[j + 1];
+            }
+
+            totalContatos--;
+            encontrado = 1;
+
+            printf("\nContato excluido com sucesso!\n");
+            break;
+        }
+    }
+
+    if (encontrado == 0)
+    {
+        printf("\nContato nao encontrado.\n");
+    }
+}
+
+int main()
+{
+    int opcao;
+
+    do
+    {
+        printf("AGENDA DE CONTATOS\n");
+        printf("1 - Incluir contato\n");
+        printf("2 - Listar contatos\n");
+        printf("3 - Consultar contato\n");
+        printf("4 - Excluir contato\n");
+        printf("5 - Sair\n");
+
+        printf("\nEscolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao)
+        {
+            case 1:
+                incluirContato();
+                break;
+
+            case 2:
+                listarContatos();
+                break;
+
+            case 3:
+                consultarContato();
+                break;
+
+            case 4:
+                excluirContato();
+                break;
+
+            case 5:
+                printf("\nEncerrando programa...\n");
+                break;
+
+            default:
+                printf("\nOpcao invalida!\n");
+        }
+
+    } while(opcao != 5);
+
+    return 0;
+}
+
